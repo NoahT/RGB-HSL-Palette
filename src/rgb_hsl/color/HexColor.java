9eb23d6438d3012fec4a3291b3e5017f0ed49456
Noah Teshima <noah.teshima@gmail.com>
2019-05-26 22:16:53 -0700

    Added base conversion

--- src/rgb_hsl/color/HexColor.java
+++ src/rgb_hsl/color/HexColor.java
@@ -26,10 +26,10 @@ public class HexColor extends RGBColor {
 	/**
 	 * Copy constructor used to create a HexColor with the same
 	 * values as the given reference.
-	 * @param color Color reference containing the RGB values to
+	 * @param color RGBColor reference containing the RGB values to
 	 * copy.
 	 */
-	public HexColor(HexColor color) {
+	public HexColor(RGBColor color) {
 		this(color.getRed(), color.getGreen(), color.getBlue());
 	}
 	
@@ -48,7 +48,8 @@ public class HexColor extends RGBColor {
 		super(red, green, blue);
 		this.updateHexCode();
 	}
-	
+
+
 	/**
 	 * Private mutator method designed to set the
 	 * instance's hexCode to its corresponding
@@ -60,14 +61,14 @@ public class HexColor extends RGBColor {
 		String redHex = Integer.toHexString(this.getRed()),
 				greenHex = Integer.toHexString(this.getGreen()),
 				blueHex = Integer.toHexString(this.getBlue());
-		
+
 		//always make sure the hex code is formatted with two characters.
 		//there will only be one chacter in the case that any value is 0-15
 		//inclusive
 		redHex = (redHex.length() == 1) ? "0" + redHex : redHex;
 		greenHex = (greenHex.length() == 1) ? "0" + greenHex : greenHex;
 		blueHex = (blueHex.length() == 1) ? "0" + blueHex : blueHex;
-		
+
 		this.hexCode = String.format("%s%s%s",
 				redHex,
 				greenHex,
@@ -75,69 +76,42 @@ public class HexColor extends RGBColor {
 	}
 
 	/**
-	 * Mutator method designed to set the
-	 * amount of red in the current color.
-	 * @param red integer value containing the
-	 * new amount of red in the current color.
+	 * Accessor method used to get the hex color ocde
+	 * associated with the current instance.
+	 * @return String object containing the current hex color
+	 * cde.
 	 */
+	public String getHexCode() {
+		return this.hexCode;
+	}
+
 	@Override
-	public void setRed(int red) {
-		this.setRed(red);
+	public void setColor(double red, double green, double blue) {
+		super.setColor(red, green, blue);
 		this.updateHexCode();
 	}
 
-	/**
-	 * Mutator method designed to set the
-	 * amount of green in the current color.
-	 * @param green integer value containing the
-	 * new amount of green in the current color.
-	 */
+	@Override
+	public void setRed(int red) {
+		super.setRed(red);
+		this.updateHexCode();
+	}
+
 	@Override
 	public void setGreen(int green) {
 		super.setGreen(green);
 		this.updateHexCode();
 	}
 
-	/**
-	 * Mutator method designed to set the
-	 * amount of blue in the current color.
-	 * @param blue integer value containing the
-	 * new amount of blue in the current color.
-	 */
 	@Override
 	public void setBlue(int blue) {
 		super.setBlue(blue);
 		this.updateHexCode();
 	}
 	
-	/**
-	 * Mutator method designed to set the amount of
-	 * red, green, and blue to the corresponding
-	 * amounts.
-	 * @param red integer value containing the amount of
-	 * red
-	 * @param green integer value containing the amount of
-	 * green
-	 * @param blue integer value containing the amount of
-	 * blue
-	 */
-	public void setColor(int red, int green, int blue) {
-		super.setColor(red, green, blue);
-		this.updateHexCode();
-	}
-	
-	/**
-	 * Accessor method used to get the corresponding hex color
-	 * code.
-	 * @return String object containing the hex color code.
-	 */
-	public String getHexColorCode() {
-		return "#" + this.hexCode;
-	}
-	
 	@Override
 	public String toString() {
-		return String.format("#%s\n",
+		return String.format("#%s",
 				this.hexCode);
 	}
 }
